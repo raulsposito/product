33f2195e014d6c0bb1d4488fd7fcdaaf9cc15a13
raulsposito <raulsposito@gmail.com>
2021-05-05 13:32:23 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: "Shirt",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNBhl4Gkk4sXMOm6_D1SbateObuAQXt2C72g&usqp=CAU"
+    }
+})
