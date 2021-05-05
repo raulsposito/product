33a69615f634748cf1cc2ef7eddfa38aebb74618
raulsposito <raulsposito@gmail.com>
2021-05-05 15:23:26 -0300

    add computed properties

--- script.js
+++ script.js
@@ -1,37 +1,52 @@
-//Create a new button and method to decrement the value of `cart`.
+//Add a new boolean data property `onSale` and create a computed property that takes `brand`, `product` and `onSale` and prints out a string whenever `onSale` is true.
 
 var app = new Vue({
     el: '#app',
     data: {
-      product: 'Socks',
-      image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
-      inStock: true,
-      details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-      variants: [
-        {
-          variantId: 2234,
-          variantColor: 'green',
-          variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
-        },
-        {
-          variantId: 2235,
-          variantColor: 'blue',
-          variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg'
-        }
-      ],
-      cart: 0
+        product: 'Socks',
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
+        details: ['80% cotton', '20% polyester', '100% fun!'],
+        variants: [
+          {
+            variantId: 2234,
+            variantColor: 'green',
+            variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+            variantQuantity: 10     
+          },
+          {
+            variantId: 2235,
+            variantColor: 'blue',
+            variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
+            variantQuantity: 0     
+          }
+        ],
+        cart: 0,
+        onSale: true
     },
     methods: {
-      addToCart() {
-        this.cart += 1
-      },
-      updateProduct(variantImage) {
-        this.image = variantImage
-      },
-      removeFromCart() {
-        this.cart -= 1
-      }
+        addToCart: function() {
+            this.cart += 1
+        },
+        updateProduct: function(index) {  
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product  
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+          if (this.onSale) {
+            return this.brand + ' ' + this.product + ' are on sale!'
+          } 
+            return  this.brand + ' ' + this.product + ' are not on sale'
+        }
     }
   })
-  
-  
